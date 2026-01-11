1aa164efa4911998d2445c192050857f85423f62
lupinus <bzirs@outlook.com>
2026-01-11 14:42:31 +0800

    modal open and close

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const btns = document.querySelectorAll('button.show-modal');
+
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const closeModalEl = document.querySelector('.close-modal');
+
+function closeModal() {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+}
+
+btns.forEach((btn, index) => {
+  btn.addEventListener('click', function () {
+    modalEl.classList.remove('hidden');
+    overlayEl.classList.remove('hidden');
+  });
+});
+
+closeModalEl.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalEl.classList.contains('hidden')) {
+    closeModal();
+  }
+});
